3764c9a8821212ede5b648888938253642acd553
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-10-02 19:30:38 -0700

    Add Item and SaleBundle DAOS

---
+++ Item.js
@@ -0,0 +1,107 @@
+const mysql = require('mysql');
+
+class ItemDAO {
+  constructor(config) {
+    this.connection = mysql.createConnection(config);
+  }
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      this.connection.connect((err) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  }
+
+  /**
+   * Disconnects from the database.
+   * @returns {Promise<void>} A promise that resolves when the
+   * connection is closed.
+   */
+  disconnect() {
+    return new Promise((resolve, reject) => {
+      this.connection.end((err) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  }
+
+  /**
+   * Creates a new item in the database.
+   * @param {*} name The name of the item.
+   * @param {*} description The description of the item.
+   * @returns {Promise<number>} The id of the newly created item.
+   */
+  createItem(name, description) {
+    return new Promise((resolve, reject) => {
+      const query = 'INSERT INTO Item (name, description) VALUES (?, ?)';
+      this.connection.query(query, [name, description], (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results.insertId);
+      });
+    });
+  }
+
+  /**
+   * Getes an item from the database by its id.
+   * @param {*} id The id of the item to get.
+   * @returns {Promise<Object>} The item with the given id.
+   */
+  getItemById(id) {
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT * FROM Item WHERE id = ?';
+      this.connection.query(query, [id], (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results[0]);
+      });
+    });
+  }
+
+  /**
+   *  Updates an item in the database.
+   * @param {*} id The id of the item to update
+   * @param {*} name The new name of the item
+   * @param {*} description The new description of the item
+   * @returns {Promise<number>} The number of affected rows.
+   */
+  updateItem(id, name, description) {
+    return new Promise((resolve, reject) => {
+      const query = 'UPDATE Item SET name = ?, description = ? WHERE id = ?';
+      this.connection.query(query, [name, description, id], (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results.affectedRows);
+      });
+    });
+  }
+
+  /**
+   * Deletes an item from the database.
+   * @param {*} id The id of the item to delete.
+   * @returns {Promise<number>} The number of affected rows.
+   */
+  deleteItem(id) {
+    return new Promise((resolve, reject) => {
+      const query = 'DELETE FROM Item WHERE id = ?';
+      this.connection.query(query, [id], (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results.affectedRows);
+      });
+    });
+  }
+}
+
+module.exports = ItemDAO;
---
+++ SaleBundle.js
@@ -0,0 +1,110 @@
+const mysql = require('mysql');
+
+class SaleBundleDAO {
+  constructor(config) {
+    this.connection = mysql.createConnection(config);
+  }
+
+  /**
+   * Connects to the database.
+   * @returns {Promise<void>} A promise that resolves when the
+   *  connection is established.
+   */
+  connect() {
+    return new Promise((resolve, reject) => {
+      this.connection.connect((err) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  }
+
+  /**
+   * Disconnects from the database.
+   * @returns {Promise<void>} A promise that resolves when the
+   * connection is closed.
+   */
+  disconnect() {
+    return new Promise((resolve, reject) => {
+      this.connection.end((err) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  }
+
+  /**
+   * Creates a new SaleBundle in the database.
+   * @param {*} expiration The expiration date of the SaleBundle.
+   * @returns {Promise<number>} The id of the newly created SaleBundle.
+   */
+  insertSaleBundle(expiration) {
+    const query = 'INSERT INTO SaleBundle (expiration) VALUES (?)';
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, [expiration], (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result);
+      });
+    });
+  }
+
+  /**
+   * Retrieves a SaleBundle by its ID.
+   * @param {number} id The ID of the SaleBundle.
+   * @returns {Promise<Object>} The SaleBundle object.
+   */
+  getSaleBundleById(id) {
+    const query = 'SELECT * FROM SaleBundle WHERE id = ?';
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, [id], (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results[0]);
+      });
+    });
+  }
+
+  /**
+   * Updates a SaleBundle's expiration date.
+   * @param {number} id The ID of the SaleBundle.
+   * @param {*} expiration The new expiration date.
+   * @returns {Promise<void>} A promise that resolves when the update is complete.
+   */
+  updateSaleBundle(id, expiration) {
+    const query = 'UPDATE SaleBundle SET expiration = ? WHERE id = ?';
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, [expiration, id], (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result);
+      });
+    });
+  }
+
+  /**
+   * Deletes a SaleBundle by its ID.
+   * @param {number} id The ID of the SaleBundle.
+   * @returns {Promise<void>} A promise that resolves when the deletion is complete.
+   */
+  deleteSaleBundle(id) {
+    const query = 'DELETE FROM SaleBundle WHERE id = ?';
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, [id], (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result);
+      });
+    });
+  }
+}
+
+module.exports = SaleBundleDAO;
